7ee3c7d0e5adbe4af8d54e99f105d7a3fbfa270d
Samadali123 <samadali0125@gmail.com>
2024-05-25 20:39:48 +0530

    add Online Db

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,14 @@
 const mongoose = require(`mongoose`);
-mongoose.connect("mongodb://127.0.0.1:27017/facebooklite");
+// mongoose.connect("mongodb://127.0.0.1:27017/facebooklite");
+
+mongoose.connect("mongodb+srv://samadali0125:Samad%40123@cluster0.jir9r0m.mongodb.net/FB?retryWrites=true&w=majority&appName=Cluster0")
+    .then(function() {
+        console.log("Db connected Successfully.")
+    }).catch(function(error) {
+        console.log("There is a Problem While connecting to a Database.")
+    })
+
+
 const passportlocalmongoose = require(`passport-local-mongoose`);
 const { stringify } = require("uuid");
 
